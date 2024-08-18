304e81fbe023efe7039007195473f563e12f614b
alonso130r <131227801+alonso130r@users.noreply.github.com>
2024-08-18 19:09:50 -0400

    update to use 6bw quantization

--- runpod/llama-3.1-405b/quantize.py
+++
@@ -1,9 +0,0 @@
-from exllamav2 import ExllamaQuantizer, ExllamaModel
-
-model = ExllamaModel.from_pretrained('/llama-405b')
-
-quantizer = ExllamaQuantizer(model)
-
-quantized_model = quantizer.quantize()
-
-quantized_model.save_pretrained('/llama-405b-quantized')
--- runpod/llama-3.1-405b/save_weights.py
+++ runpod/llama-3.1-405b/save_weights.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 token = os.getenv("HUGGINGFACE")
-model_name = "meta-llama/Meta-Llama-3.1-405B"
+model_name = "ek826/Meta-Llama-3.1-405B-Instruct-6.0bpw-exl2"
 custom_cache_dir = "./llama-405b"
 
 model_config = hf_hub_download(repo_id=model_name, filename="config.json", cache_dir=custom_cache_dir, token=token)
@@ -15,6 +15,8 @@ special_tokens =  hf_hub_download(repo_id=model_name, filename="special_tokens_m
 tokenizer = hf_hub_download(repo_id=model_name, filename="tokenizer.json", cache_dir=custom_cache_dir, token=token)
 index = hf_hub_download(repo_id=model_name, filename="model.safetensors.index.json", cache_dir=custom_cache_dir, token=token)
 
+patch = hf_hub_download(repo_id=model_name, filename="patch.diff", cache_dir=custom_cache_dir, token=token)
+
 weights = []
-for i in range(1, 191):
-    weights.append(hf_hub_download(repo_id=model_name, filename=f"model-{i:05}-of-00191.safetensors", cache_dir=custom_cache_dir, token=token))
+for i in range(1, 38):
+    weights.append(hf_hub_download(repo_id=model_name, filename=f"output-{i:05}-of-00038.safetensors", cache_dir=custom_cache_dir, token=token))
